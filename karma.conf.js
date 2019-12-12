ca1c10318558166c6dbbd407577afd289fee529e
Nick <zhongcheng509@gmail.com>
2019-12-12 18:59:37 +0800

    拷贝单元测试

---
+++ karma.conf.js
@@ -0,0 +1,16 @@
+const webpackConfig = require('@vue/cli-service/webpack.config.js')
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha'],
+    files: [
+      'tests/**/*.spec.js',   //tests目录下，所有.spce.js结尾的测试文件
+      // 'tests/**/*.test.js'
+    ],
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap']
+    },
+    webpack: webpackConfig,
+    reporters: ['spec'],
+    browsers: ['ChromeHeadless']
+  })
+}
--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,13 +1,82 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/Button.vue'
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { mount, shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
+import Button from '@/Button'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+chai.use(sinonChai)
+
+Vue.config.productionTip = false // 阻止生产环境提示
+Vue.config.devtools = false // 是否允许devtools检查代码
+
+describe('Button', () => {
+  it('存在.', () => {
+    expect(Button).to.be.ok
+  })
+  it('可以设置icon.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+      }
     })
-    expect(wrapper.text()).to.include(msg)
+    const useElement = wrapper.find('use')
+    expect(useElement.attributes()['href']).to.equal('#icon-settings')
+  })
+  it('可以设置loading.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
+    const vm = wrapper.vm
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
+    vm.$destroy()
+  })
+  it('icon 默认的 order 是 1', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('设置 iconPosition 可以改变 order', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('点击 button 触发 click 事件', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+      }
+    })
+
+    const vm = wrapper.vm
+
+    const callback = sinon.fake() // callback知道自己有没有被调用过
+    vm.$on('click', callback)
+    vm.$el.click()
+    expect(callback).to.have.been.called
   })
 })
